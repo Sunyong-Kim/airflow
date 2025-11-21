c8154de9ef33def462b78d7cbeb3297f5cfedc31
Sunyong-Kim <flame9880@gmail.com>
2025-11-21 15:24:01 +0900

    email operator upload

---
+++ dags/dags_email_operator.py
@@ -0,0 +1,17 @@
+from airflow.sdk import DAG
+import datetime
+import pendulum
+from airflow.providers.smtp.operators.smtp import EmailOperator
+
+with DAG(
+    dag_id="dags_email_operator",
+    schedule="0 8 1 * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    send_email_task = EmailOperator(
+        task_id="send_email_task",
+        to="sykim@vicglobal.co.kr",
+        subject="Airflow Success Email",
+        html_content="Airflow 작업이 완료되었습니다",
+    )
