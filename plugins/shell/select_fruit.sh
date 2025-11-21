7d2cb6e6769969349d40ca86a2e832f572031c45
Sunyong-Kim <flame9880@gmail.com>
2025-11-21 14:42:25 +0900

    select fruit upload

--- plugins/shell/select_fruit.sh
+++ plugins/shell/select_fruit.sh
@@ -1,19 +1,10 @@
 FRUIT=$1
 if [ $FRUIT == APPLE ];then
-<<<<<<< HEAD
         echo "You selected Apple!"
-=======
-	echo "You selected Apple!"
->>>>>>> 4d0bfbf253fa4a70ce566ccced421fef6d7beea2
 elif [ $FRUIT == ORANGE ];then
         echo "You selected Orange!"
 elif [ $FRUIT == GRAPE ];then
         echo "You selected Grape!"
 else
-<<<<<<< HEAD
         echo "You selected other Fruit!"
 fi
-=======
-	echo "You selected other Fruit!"
-fi
->>>>>>> 4d0bfbf253fa4a70ce566ccced421fef6d7beea2
